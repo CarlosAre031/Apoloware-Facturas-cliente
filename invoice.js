3e5e88c7d63e4bab349c0304f1213540ae1427c9
Jhan Carlos Arevalo <134398105+CarlosAre031@users.noreply.github.com>
2024-09-20 20:08:31 -0500

    Update invoice.js

--- invoice.js
+++ invoice.js
@@ -31,7 +31,7 @@ function generatePDF() {
     // Función para ajustar el contenido si es demasiado largo
     function adjustContent() {
         const currentHeight = elementCopy.offsetHeight;
-        const maxHeight = 950 - 0; // 279.4mm (altura carta) - 20mm (margen total)
+        const maxHeight = 900 - 0; // 279.4mm (altura carta) - 20mm (margen total)
         if (currentHeight > maxHeight) {
             const scale = maxHeight / currentHeight;
             elementCopy.style.transform = `scale(${scale})`;
